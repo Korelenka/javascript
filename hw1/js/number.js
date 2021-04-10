b1f915b00031a60821b4a68a33b6f3b8fced7cc5
Korelenka <76471461+Korelenka@users.noreply.github.com>
2021-04-10 12:02:06 +0300

    hm2

--- hw1/js/number.js
+++ hw1/js/number.js
@@ -3,9 +3,19 @@ let n=98 ;
 let arr = n.toString().split('').map(Number)
 console.log(arr.reduce( (a,b) => a+b ));
 
+let m=98;
+let sum = m % 10 + (m - m % 10) / 10;
+console.log(sum);
+
+
 let a = 3, b = 5;
 b = [a, a = b][0]
 console.log(a,b);
+
+a = a + b;
+b = a - b;
+a = a - b;
+console.log(a, b);
 
 let c = 3, d = 5, g = 8;
 let res = 2*(c*d) + (d*g) + (c*g);
---
+++ hw2/js/number.js
@@ -0,0 +1,41 @@
+let count = 4;
+if (count <= 100 && count >= 90) {
+    console.log("отлично");
+} else if (count <= 89 && count >= 60) {
+    console.log("хорошо");
+} else if (count <= 59 && count >= 40) {
+    console.log("удовлетворительно");
+} else (count <= 39 && count >= 0); {
+    console.log("попробуй потом")
+    }
+
+    let a = 3, b = 4, c = 9;
+    switch (c) {
+        case 3:
+            c = a + b ;
+            break;
+        case 5:
+            c = a - b ;
+            break;
+        case 7:
+            c = a * b ;
+            break;
+        case 9:
+            c = a / b ;
+            break;
+    }
+    console.log(c);
+
+    for (let i = 2; i >= 0 && i < 145 ; i += 2) {
+        console.log(i);    
+    }
+
+       
+     
+    
+    for (let i = 1; i >= 0 && i < 10; i += 1) {
+        console.log(i);
+     i * 1 ;
+     
+    }
+    
